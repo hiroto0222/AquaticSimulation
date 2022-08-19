b5cd37cd176f17635e8d77c3cfa9ac1ea912dea2
hiroto0222 <aoyamahiroto0222@gmail.com>
2022-08-19 18:36:42 +0900

    feat: add colored textures to noiseMap

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MapGenerator : MonoBehaviour {
+    public enum DrawMode { NoiseMap, ColorMap };
+    public DrawMode drawMode;
+
     public int mapWidth;
     public int mapHeight;
     public float noiseScale;
@@ -17,12 +20,33 @@ public class MapGenerator : MonoBehaviour {
 
     public bool autoUpdate;
 
+    public TerrainType[] terrains;
+
     public void GenerateMap() {
         float[,] noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, noiseScale, seed, octaves, persistance, lacunarity, offset);
+        Color[] colorMap = new Color[mapWidth * mapHeight];
+
+        for (int y = 0; y < mapHeight; y++) {
+            for (int x = 0; x < mapWidth; x++) {
+                float currHeight = noiseMap[x, y];
+                foreach (TerrainType terrain in terrains) {
+                    if (currHeight <= terrain.height) {
+                        colorMap[y * mapWidth + x] = terrain.color;
+                        break;
+                    }
+                }
+            }
+        }
 
         MapDisplay display = FindObjectOfType<MapDisplay>();
 
-        display.DrawNoiseMap(noiseMap);
+        if (drawMode == DrawMode.NoiseMap) {
+            Texture2D noiseTexture = TextureGenerator.TextureFromNoiseMap(noiseMap);
+            display.DrawTexture(noiseTexture);
+        } else if (drawMode == DrawMode.ColorMap) {
+            Texture2D colorTexture = TextureGenerator.TextureFromColorMap(colorMap, mapWidth, mapHeight);
+            display.DrawTexture(colorTexture);
+        }
     }
 
     // clamp vars
@@ -32,4 +56,11 @@ public class MapGenerator : MonoBehaviour {
         if (lacunarity < 1) lacunarity = 1;
         if (octaves < 0) octaves = 0;
     }
+}
+
+[System.Serializable]
+public struct TerrainType {
+    public string name;
+    public float height;
+    public Color color;
 }
